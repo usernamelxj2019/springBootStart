9db60dbf36495cb597ea49fb74d3b626788973e9
michelangelo <914711550@qq.com>
2018-08-04 11:02:17 +0800

    add log

--- springbootstart/src/main/java/com/michelangelo/cn/example/demo/DemoApplication.java
+++ springbootstart/src/main/java/com/michelangelo/cn/example/demo/DemoApplication.java
@@ -9,5 +9,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("start.....");
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("start  success .....");
 	}
 }

5ab1e852822c29ccac0fbeeabfb8e9290ecadc19
michelangelo <914711550@qq.com>
2018-08-05 10:47:29 +0800

    add profile

---
+++ springbootstart/src/main/java/com/michelangelo/cn/example/controller/SampleController.java
@@ -0,0 +1,13 @@
+package com.michelangelo.cn.example.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}
--- springbootstart/src/main/java/com/michelangelo/cn/example/demo/DemoApplication.java
+++ springbootstart/src/main/java/com/michelangelo/cn/example/demo/DemoApplication.java
@@ -2,9 +2,23 @@ package com.michelangelo.cn.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
+
 public class DemoApplication {
+/*
+    SpringbootApplication 类继承 SpringBootServletInitializer 并重写 configure 方法
+
+	@SpringBootApplication
+	public class SpringbootApplication extends SpringBootServletInitializer {
+
+		@Override
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+			return application.sources(SpringbootApplication.class);
+		}*/
+
 
 	public static void main(String[] args) {
 		System.out.println("start.....");

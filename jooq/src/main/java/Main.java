bbec47876c8ba1562182648b5ef274ed8f4083a9
chenshifeng <chenshifeng@zhixueyun.com>
2019-02-27 16:30:20 +0800

    add gitignore

--- jooq/src/main/java/Main.java
+++ jooq/src/main/java/Main.java
@@ -11,6 +11,7 @@ import java.sql.DriverManager;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("222222222222222222");
 
 
         System.out.println("hello.....");
